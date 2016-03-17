f78c8a4aa299efd27d8eefcff1e2e30ad1b73b11
Andrew Thomas <athomas@scribenet.com>
2016-03-17 15:20:25 -0400

    Updating welcome layout and adding additional styles

--- config/app.php
+++ config/app.php
@@ -156,6 +156,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        PhpConsole\Laravel\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
+
     ],
 
     /*
@@ -201,6 +205,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var elixir = require('laravel-elixir');
 
+require('laravel-elixir-imagemin');
+require('laravel-elixir-sass-compass');
+
+elixir.config.images = {
+    folder: 'images',
+    outputFolder: 'img'
+};
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,5 +20,31 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.compass("app.scss", "public/css", {
+  		style: "compressed",
+  		sass: "resources/assets/sass",
+  		font: "public/fonts",
+  		image: "public/img",
+  		javascript: "public/js",
+  		sourcemap: true
+ 		});
+    mix.scripts('../../../node_modules/jquery/dist/jquery.js', 'public/js/jquery.js')
+       .scripts('../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', 'public/js/bootstrap.js')
+       .scripts('../../../node_modules/flexslider/jquery.flexslider.js', 'public/js/flexslider.js')
+       .scripts('../../../node_modules/desandro-classie/classie.js', 'public/js/classie.js')
+       .scripts('../../../node_modules/waypoints/lib/jquery.waypoints.js', 'public/js/waypoints.js')
+       .scripts('../../../node_modules/retina.js/src/retina.js', 'public/js/retina.js')
+       .scripts(['scripts.js']);
+    mix.imagemin();
+
+    mix.copy('node_modules/et-line/fonts/', 'public/fonts')
+       .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts')
+       .copy('node_modules/flexslider/fonts/', 'public/fonts')
+       .copy('node_modules/font-awesome/fonts/', 'public/fonts')
+       .copy('node_modules/animate.css/animate.min.css', 'public/css');
+    
+    mix.browserSync({
+        proxy: 'ngd.app',
+        xip: true
+    });
 });
